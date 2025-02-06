7acd61f5106d061c42c3b8e8f77b38f27a5bf7ad
lukashenko Vladimir <lukashenko200440@gmail.com>
2025-02-06 18:45:07 +0300

    remove attackDto
    was an idea. Useless now

--- DTOs/AttackDto.py
+++
@@ -1,8 +0,0 @@
-import uuid
-
-class AttackDto:
-    def __init__(self, damageDto, userDto, dateTime):
-        self.id = uuid.uuid1
-        self.damageDto = damageDto
-        self.userDto = userDto
-        self.dateTime = dateTime
